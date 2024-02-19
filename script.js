cbb214c3f4124e226c60afee1de96b91073ab142
Swagatam Adhikary <133666171+swagatamadhikary018@users.noreply.github.com>
2024-02-19 14:13:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed  = new Typed(".multiple-text", {
+    strings: ["Fresher","Frontend Developer","Web Developer","Programmer","YouTuber","Gamer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    typeDelay: 1000,
+    loop: true
+
+
+})
